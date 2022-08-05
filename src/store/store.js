b75dadf5363d93dae889913266f4dc2f44353ce6
JohnPosada <johnposada2000@gmail.com>
2022-08-05 15:40:26 -0500

    added pokemons and todo slice and  middleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { todosApi } from "./api/todosApi";
 import { counterSlice } from "./slices/counter/counterSlices";
+import { pokemonSlice } from "./slices/pokemon/pokemon";
 
 export const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: {
+    counter: counterSlice.reducer,
+    pokemons: pokemonSlice.reducer,
+    [todosApi.reducerPath]: todosApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 });
